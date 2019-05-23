0299de676c363c3c0248eb12955dbf6099087a2c
BACA VARGAS <BACA VARGAS@LAPTOP-5GR61H8V>
2019-05-22 19:46:28 -0500

    cambiar servidor

---
+++ Documents/NetBeansProjects/mercasur-master/src/clases/i_datos_bd.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author STEVEN
+ */
+public interface i_datos_bd {
+
+
+    String bd = "mercasur";
+    String user = "root_lsp";
+    String password = "root/*123";
+    String servidor = "192.168.1.12"; 
+
+
+}
